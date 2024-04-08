33bc2394fd41cc9f1e6303ec9341952fb895ecd4
jkumari25 <jagriti.sj15@gmail.com>
2024-04-08 13:20:18 +0530

    About Page UI Completed

--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
+import AboutComponent from "@/components/AboutComponent";
+import CatalogueSection from "@/components/CatalogueSection";
+import CatalogueSwiperSection from "@/components/CatalogueSwiperSection";
+import CompanySection from "@/components/CompanySection";
+import ContactSection from "@/components/ContactSection";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <CompanySection />
+      <AboutComponent />
+      <CatalogueSection />
+      <CatalogueSwiperSection />
+      <ContactSection />
     </>
   );
 }
